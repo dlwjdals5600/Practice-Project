71d35476f762ce6762dc401ce93c18a8f8cb7117
이정민 <dlwjdals5600@gmail.com>
2021-09-19 12:53:50 +0900

    Create : users app admin.py

--- users/models.py
+++ users/models.py
@@ -3,14 +3,49 @@ from django.db import models
 
 class User(AbstractUser):
 
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, ("Female")),
+        (GENDER_OTHER, ("Other")),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
+
+    LOGIN_EMAIL = 'email'
+    LOGIN_GITHUB = 'github'
+    LOGIN_KAKAO = 'kakao'
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, 'Email'),
+        (LOGIN_GITHUB, 'Github'),
+        (LOGIN_KAKAO, 'Kakao')
+    )
+
     avatar = models.ImageField(blank=True)
-    gender = models.CharField(max_length=10, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(max_length=2, blank=True)
-    currency = models.CharField(max_length=3, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW)
     superhost = models.BooleanField(default=False)
-    login_method = models.CharField(max_length=50)
+    login_method = models.CharField(max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
 
     class Meta:
         db_table = 'users'

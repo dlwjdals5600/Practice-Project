e1ad68803f897e12cb5b1faac63cfafee20cea13
이정민 <dlwjdals5600@gmail.com>
2021-10-15 17:50:14 +0900

    Create : room_list.html - css추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing:{ // 왼쪽은 class name, 오른쪽은 css
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        "xl": "1.5rem"
+      }
+    },
   },
   variants: {
     extend: {},

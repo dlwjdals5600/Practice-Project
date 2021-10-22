24dd853286ba6015a7627e7286176419e8256a30
이정민 <dlwjdals5600@gmail.com>
2021-10-22 15:48:58 +0900

    Create : create_review

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path('users/',include('users.urls', namespace='users')),
     path('rooms/',include('rooms.urls', namespace='rooms')),
     path('admin/', admin.site.urls),
-    path("reservations/",include('reservations.urls', namespace='reservations'))
+    path("reservations/",include('reservations.urls', namespace='reservations')),
+    path("reviews/",include('reviews.urls', namespace='reviews')),
 ]
 
 if settings.DEBUG:

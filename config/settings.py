e4d2e3c449f91eb1a6ea3a0aaf9f82893e7d5ac9
이정민 <dlwjdals5600@gmail.com>
2021-10-24 11:47:27 +0900

    Create:allowed_hosts2

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ DJANGO_APPS = [
     'django.contrib.staticfiles',
 ]
 
-THIRD_PARTY_APPS = ['django_countries', 'django_seed']
+THIRD_PARTY_APPS = ['django_countries', 'django_seed', 'psycopg2']
 
 PROJECT_APPS = [
     'core.apps.CoreConfig',
